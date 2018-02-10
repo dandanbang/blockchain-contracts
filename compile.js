885fb1e0b51de07d1e57c4d71fef261132f58b36
dandanbang <daniel@berkeley.ischool.edu>
2018-02-10 10:53:50 -0800

    installed mocha, web3, ganache, also writing some simple test code

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require('path'); //help us build a paths. guarantee paths compatibility across compiling environment
+const fs = require('fs'); //read in the content of the file
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //always get set to the current working directory
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts.[':Inbox'];
---
+++ test/inbox.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //whenever we are working with constructor function, we capitalize it. Purpose of one instance = one network connection.
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+
+beforeEach(async () => {
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // use one of the accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
+});
